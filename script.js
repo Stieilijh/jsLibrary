6ad0c359e49c99f5f8dd6a12e8ca3a4f7d91a345
Stieilijh <saitushar02@gmail.com>
2022-04-26 16:40:17 +0530

    Display Books done, removebtn and have read button works

--- script.js
+++ script.js
@@ -25,37 +25,65 @@ function addBook(newBook){
 }
 
 function removeBook(book){
-    myLibrary=myLibrary.filter((bk)=>bk.name!==book.name);
+
 }
 function getBook(name){
     return myLibrary.find((book)=>book.name===name);
 }
 
-window.onload=function(){
-//user interface
-const addbtn=document.querySelector("#addbtn");
-for(let i=0;i<myLibrary.length;i++){
-const books = document.querySelector(".books");
-const book = document.createElement("div");
-books.appendChild(book);
-const bookName = document.createElement("p");
-book.appendChild(bookName);
-bookName.textContent=myLibrary[i].name;
-const bookAuthor = document.createElement("p");
-book.appendChild(bookAuthor);
-bookAuthor.textContent=myLibrary[i].author;
-const bookPages = document.createElement("p");
-book.appendChild(bookPages);
-bookPages.textContent=myLibrary[i].pages;
-const readText = document.createElement("div");
-book.appendChild(readText);
-readText.textContent="Read";
-const bookRead = document.createElement("input");
-book.appendChild(bookRead);
-bookRead.type="checkbox";
-bookRead.checked=myLibrary[i].haveRead;
-const removeBook = document.createElement("button");
-book.appendChild(removeBook);
-removeBook.textContent="Remove";
+function displayBooks(){
+    const books = document.querySelector(".books");
+    books.innerHTML="";
+    for(let i=0;i<myLibrary.length;i++){
+        //book box
+        const book = document.createElement("div");
+        book.className="book";
+        books.appendChild(book);
+        //name
+        const bookName = document.createElement("p");
+        book.appendChild(bookName);
+        bookName.textContent=myLibrary[i].name;
+        //author
+        const bookAuthor = document.createElement("p");
+        book.appendChild(bookAuthor);
+        bookAuthor.textContent=myLibrary[i].author;
+        //pages
+        const bookPages = document.createElement("p");
+        book.appendChild(bookPages);
+        bookPages.textContent=myLibrary[i].pages;
+        //text for the checkbox
+        const readText = document.createElement("div");
+        book.appendChild(readText);
+        readText.textContent="Read";
+        //checkbox
+        const bookRead = document.createElement("input");
+        book.appendChild(bookRead);
+        bookRead.type="checkbox";
+        bookRead.checked=myLibrary[i].haveRead;
+        bookRead.addEventListener("change",()=>{
+            if(bookRead.checked){
+                myLibrary[i].haveRead=true;
+            }else{
+                myLibrary[i].haveRead=false;
+            }
+            console.log(myLibrary);
+        });
+        //remove button
+        const removeBookbtn = document.createElement("button");
+        book.appendChild(removeBookbtn);
+        removeBookbtn.textContent="Remove";
+        removeBookbtn.addEventListener("click",()=>{
+            myLibrary=myLibrary.filter((bk)=>bk.name!==myLibrary[i].name);
+            displayBooks();
+        });
+    }
 }
+
+window.onload=function(){
+const addbtn = document.querySelector("#addbtn");
+addbtn.addEventListener("click",()=>{
+    
+});
+displayBooks();
+
 }
